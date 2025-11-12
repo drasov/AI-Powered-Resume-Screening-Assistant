0363782a4652f8fd5eb81f6da481832f136dd588
Your Namgit config --global user.email <drasov@myseneca.ca>
2025-11-12 17:29:48 -0500

    Improved regex patterns for PII masking

--- scripts/resumes.py
+++ scripts/resumes.py
@@ -3,7 +3,7 @@ import pathlib as Path
 import re
 
 # File paths
-file_path = "data/resumes/sample_resumes.csv"
+file_path = "../data/resumes/sample_resumes.csv"
 file_path_anonymized = "C:/Users/vokod/.cache/kagglehub/datasets/saugataroyarghya/resume-dataset/versions/1/resume_data_anonymized2.csv"
 
 # Load Data and take only first 20 for testing
@@ -11,10 +11,11 @@ df = pd.read_csv(file_path).head(20)
 df_anonymized = df.copy()
 
 # Regex patterns for anonymization
-Email = re.compile(r'[a-zA-Z0-9._%+-]+@[a-z-A-Z0-9.-]+\.[a-zA-Z]{2,}')
+Email = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')
 Name = re.compile(r'(?<=Name:\s).*?(?=\n|$)')
-Phone = re.compile(r'\+?\b(?:\+?\s*\d{1,3}[-.\s]?)?(?:\(?\d{2,4}\)?[-.\s]?)?\d{3}[-.\s]?\d{4}\b')
-Address = re.compile(r'\d{1,5}\s\w+\s\w+\.?')
+Phone = re.compile(r'(?<=Phone:\s).*?(?=\n|$)')
+Address = re.compile(r'(?<=Address:\s).*?(?=\n|$)')
+
 URL = re.compile(r'(https?://[^\s]+)')
 
 # Anonymization function
@@ -27,10 +28,15 @@ def anonymize_resume(resume_text: str) -> str:
 
     return resume_text
 
-# Apply anonymization
-for col in df_anonymized.columns:
-    df_anonymized[col] = df_anonymized[col].astype(str).apply(anonymize_resume)
+# # Apply anonymization
+# for col in df_anonymized.columns:
+#     df_anonymized[col] = df_anonymized[col].astype(str).apply(anonymize_resume)
 
-# Save anonymized resumes
-df_anonymized.to_csv(file_path_anonymized, index=False)
-print("Anonymized resumes saved to:", file_path_anonymized)
+# # Save anonymized resumes
+# df_anonymized.to_csv(file_path_anonymized, index=False)
+# print("Anonymized resumes saved to:", file_path_anonymized)
+
+for i, row in df.iterrows():
+    print(f"\n--- Resume {i+1} ---")
+    print("\nAnonymized:\n", anonymize_resume(row['resume_text']))
+    print("-----------------------------\n")
